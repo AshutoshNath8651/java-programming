4b416d07ebb098d935047532a8118ecb0035c56d
Ashutosh <ashutoshnath2007@gmail.com>
2026-02-11 23:12:01 +0530

    test file change

--- Test.java
+++ Test.java
@@ -1,9 +1,8 @@
 public class Test {
     public static void main(String[] args) {
     Car obj1=new Car("red","harrier");
-    Car obj2=new Car("blue","harrier");
-    Car obj3=new Car("pink","harrier");
-    Car[] car= {obj1, obj2, obj3};
+    
+
     
 
         
